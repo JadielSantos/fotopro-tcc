eea72bc51d4b5c0f8e8a8862c2f7667ab3aaa7e2
Jadiel dos Santos <jadieldossantos@ioasys.com.br>
2025-03-29 17:09:38 -0300

    feat: implemented neural network model training

--- FaceRecognition/train_model.py
+++ FaceRecognition/train_model.py
@@ -6,6 +6,9 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix
 from mlxtend.plotting import plot_confusion_matrix
+from keras._tf_keras.keras.utils import to_categorical
+from keras._tf_keras.keras import models
+from keras._tf_keras.keras import layers
 
 def load_embeddings(filename):
     df = pd.read_csv(filename)
@@ -20,13 +23,33 @@ def discretize_labels(y):
     y = encoder.transform(y)
     return y
 
-def train_knn(X, y, valX):
+def train_knn(trainX, trainY, valX, valY):
     model = KNeighborsClassifier(n_neighbors=5)
-    model.fit(X, y)
-    yhat_train = model.predict(X)
+    model.fit(trainX, trainY)
+    yhat_train = model.predict(trainX)
     yhay_val = model.predict(valX)
-    print_confusion_matrix('KNN', y, yhat_train)
+    print_confusion_matrix('KNN', trainY, yhat_train)
     return yhay_val
+
+def train_nn(trainX, trainY, valX, valY):
+    trainY = to_categorical(trainY)
+    valY = to_categorical(valY)
+    model = models.Sequential()
+    model.add(layers.Dense(64, activation='relu', input_shape=(trainX.shape[1],)))
+    model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(4, activation='softmax')) # 4 people in the dataset
+    model.summary()
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    model.fit(trainX, trainY, epochs=100, batch_size=8, verbose=1)
+    yhat_train = model.predict(trainX)
+    yhat_val = model.predict(valX)
+    yhat_train = np.argmax(yhat_train, axis=1)
+    yhat_val = np.argmax(yhat_val, axis=1)
+    valY = np.argmax(valY, axis=1)
+    print_confusion_matrix('NN', valY, yhat_train)
+    model.save('nn_model.h5')
+    
+    return model
 
 def plot_embeddings(X, y):
     plt.figure(figsize=(10, 10))
@@ -36,11 +59,13 @@ def plot_embeddings(X, y):
     plt.show()
     
 def print_confusion_matrix(model_name, valY, yhat_val):
-    cm = confusion_matrix(valY, yhat_val)
+    # Found input variables with inconsistent numbers of samples: [170, 174]
+    cm = confusion_matrix(valY, yhat_val, labels=[0, 1, 2, 3])
     total = sum(sum(cm))
-    acc = (cm[0, 0] + cm[1, 1]) / total
-    sensitivity = cm[0, 0] / (cm[0, 0] + cm[0, 1])
-    specificity = cm[1, 1] / (cm[1, 0] + cm[1, 1])
+    # 4 people in the dataset
+    acc = (cm[0, 0] + cm[1, 1] + cm[2, 2] + cm[3, 3]) / total
+    sensitivity = cm[1, 1] / (cm[1, 0] + cm[1, 1] + cm[1, 2] + cm[1, 3])
+    specificity = cm[1, 1] / (cm[1, 0] + cm[1, 1] + cm[1, 2] + cm[1, 3])
     
     print("Model: " + model_name)
     print("Accuracy: {:.4f}".format(acc))
@@ -48,14 +73,15 @@ def print_confusion_matrix(model_name, valY, yhat_val):
     print("Specificity: {:.4f}".format(specificity))
     
     fig, ax = plot_confusion_matrix(conf_mat=cm, figsize=(5, 5))
-    plt.show()    
+    plt.show()
     
-def execute(filename):
+def initiate_model_training(filename):
     trainX, trainY = load_embeddings(filename)
     trainY = discretize_labels(trainY)
     valX, valY = load_embeddings('faces-embeddings-val.csv')
     valY = discretize_labels(valY)
-    train_knn(trainX, trainY, valX)
+    # train_knn(trainX, trainY, valX, valY)
+    train_nn(trainX, trainY, valX, valY)
     
 if __name__ == '__main__':
-    execute('faces-embeddings-train.csv')
+    initiate_model_training('faces-embeddings-train.csv')
